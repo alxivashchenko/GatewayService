03140e7dd423677ed8da1a9a16fd2041b2f47ba8
alxiv <alx.ivashchenko@gmail.com>
2026-04-04 10:33:45 +0200

    Added List of frontend urls

--- src/main/java/com/alexivashchenko/gateway/service/GatewayServiceApplication.java
+++ src/main/java/com/alexivashchenko/gateway/service/GatewayServiceApplication.java
@@ -2,12 +2,14 @@ package com.alexivashchenko.gateway.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 
 }
--- src/main/java/com/alexivashchenko/gateway/service/config/CorsGlobalConfiguration.java
+++ src/main/java/com/alexivashchenko/gateway/service/config/CorsGlobalConfiguration.java
@@ -12,15 +12,21 @@ import java.util.List;
 @Configuration
 public class CorsGlobalConfiguration {
 
-    @Value("${app.cors.allowed-origins}")
-    private List<String> allowedOrigins;
+    private final CorsProperties corsProperties;
+
+    public CorsGlobalConfiguration(CorsProperties corsProperties) {
+        this.corsProperties = corsProperties;
+    }
 
     @Bean
     public CorsWebFilter corsWebFilter() {
+
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(allowedOrigins); // for Docker "http://localhost:3000"
-//        config.setAllowedOrigins(List.of("http://localhost:5173")); // frontend URL  dev
-//        config.setAllowedOrigins(List.of("http://localhost:4173")); // frontend URL
+        config.setAllowedOrigins(corsProperties.getAllowedOrigins());
+
+//        "http://localhost:3000" //  for Docker
+//        "http://localhost:5173" // frontend URL  dev
+//        "http://localhost:4173" // frontend URL
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // HttpOnly cookies require this
